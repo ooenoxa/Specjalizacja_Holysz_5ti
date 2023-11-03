2298ea2338ac5e558ce7279a0dd8f36a8fa1b622
ooenoxa <136166751+ooenoxa@users.noreply.github.com>
2023-11-03 10:46:23 +0100

    Add files via upload

---
+++ php_02_logowanie/php_02_logowanie/app/calc_view.php
@@ -0,0 +1,42 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator rat kredytu</title>
+<link href="<?php print(_APP_URL);?>/app/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="id_kwota">Podaj kwotę kredytu: </label>
+	<input id="id_kwota" type="text" name="kwota" value="<?php isset($kwota)?print($kwota):""  ?>" />
+	<br />
+	<label for="id_procent">Podaj procent kredytu: </label>
+	<input id="id_procent" type="text" name="procent" value="<?php isset($procent)?print($procent):"" ?>" /><br />
+	<label for="id_rata">Podaj ilość rat kredytu: </label>
+	<input id="id_rata" type="text" name="rata" value="<?php isset($rata)?print($rata):"" ?>" /><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($wynik)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Wynik: '.$wynik; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
